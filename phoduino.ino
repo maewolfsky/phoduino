c03b689e659d7726cb9be2459a154d843e651b15
Matthew Baxa <matthew@baxa.info>
2012-08-11 21:05:53 -0500

    Initial version

---
+++ phoduino.ino
@@ -0,0 +1,104 @@
+/*
+ Intervalometer, possible future rotation/panning with stepper motor?
+ 
+ Borrowing heavily from http://openmoco.org/node/88
+ 
+ */
+
+// Optocoupler
+const int shutter = 13;  
+
+int interval; //interval between each frame (in ms)
+const int exposure = 1000; //exposure of each frame
+
+int frames;    // total number of frames
+const bool dumbmode = true; //dumb intervalometer
+
+// Below here are values for advanced method where phoduino controls exposure
+
+// last time our camera fired
+unsigned long last_tm = 0;
+
+// counter for how long we've been exposing
+unsigned long exp_tm  = 0;
+
+// whether or not we're currently exposing
+bool exposing = false;
+
+
+
+void setup()  { 
+  pinMode(shutter, OUTPUT);
+  interval = 2000; // default value: 2 seconds
+  frames = 5;      // default value: 5 frames     
+  Serial.begin(9600); // for debugging output
+} 
+
+// Send an open/close to the shutter. Used in basic intervalometer mode
+void triggerShutter() {
+  //if the shutter length isn't specified, just open and close the shutter
+  triggerShutter(0);
+}
+
+// Open the shutter for the specified length in ms
+void triggerShutter(int ms) {
+  digitalWrite(shutter, HIGH);
+  Serial.println("Shutter open");
+  delay(ms);
+  digitalWrite(shutter, LOW);
+  Serial.println("Shutter closed");
+}
+
+// Perhaps we want to adjust the interval as we go along to make the 
+//  timelapse speed up (longer interval) or slow down (shorter interval)
+void takePictures( int num_frames, int interval ) {
+  for (int i = 0; i < num_frames; i++) {
+    triggerShutter();
+    delay(interval);
+  }
+}
+
+// the loop routine runs over and over again forever:
+void loop()  { 
+  
+  if(dumbmode) {
+    takePictures(frames,interval);
+  }
+  
+  //Advanced stuff here for when phoduino is controlling shutter length
+  // and to allow other work to be done
+  // do some setup code here
+  if ( exposing == false && millis() - last_tm > interval ) {
+    //reset exposure timer
+    exp_tm = millis();
+    
+    //set flag to indicate that we're currently exposing
+    exposing = true;
+    Serial.println(" Opening shuter");
+    digitalWrite(shutter, HIGH);
+  }
+  else if (exposing == true && millis() - exp_tm > exposure) {
+     // if we're currently exposing, and our exposure
+      // time has elapsed...
+     
+      // disable optocoupler
+    Serial.println(" Closing shutter");
+    digitalWrite(shutter, LOW);
+
+      // we set this now to ensure that our
+      // interval time is measured from the
+      // time an image is completed until the
+      // time the next one is triggered
+      //
+      // if you want the interval time to be
+      // measured between the time the image
+      // is triggered and the next image is
+      // triggered, move this to before
+      // bringing the camera pin high
+     
+    last_tm = millis();
+   
+      // reset exposing flag
+    exposing = false;
+  }
+}
